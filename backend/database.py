335300c4f152d7a8e57f5fb1c226d1fc25b99df8
ephraimjude-t <ephraimjude.t@gmail.com>
2026-04-22 23:18:02 +0530

    final

--- backend/database.py
+++ backend/database.py
@@ -1,38 +1,59 @@
-import aiosqlite
+import os
+import asyncio
+import asyncpg
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+async def get_pool():
+
+    return await asyncpg.create_pool(dsn=DATABASE_URL)
 
 async def initial_setup():
-    async with aiosqlite.connect('candles.db') as db:
-        await db.execute("""
+    conn = await asyncpg.connect(DATABASE_URL)
+    try:
+        await conn.execute("""
             CREATE TABLE IF NOT EXISTS candles (
                 symbol    TEXT,
-                time      INTEGER,
-                open      REAL,
-                high      REAL,
-                low       REAL,
-                close     REAL,
-                volume    REAL,
+                time      BIGINT,
+                open      DOUBLE PRECISION,
+                high      DOUBLE PRECISION,
+                low       DOUBLE PRECISION,
+                close     DOUBLE PRECISION,
+                volume    DOUBLE PRECISION,
                 PRIMARY KEY (symbol, time)
             )
         """)
-        await db.commit()
+    finally:
+        await conn.close()
 
 async def insert_data(symbol, candle):
-    async with aiosqlite.connect('candles.db') as db:
-        await db.execute("""
-            INSERT OR REPLACE INTO candles VALUES (?, ?, ?, ?, ?, ?, ?)
-        """, (symbol, candle['time'], candle['open'], candle['high'], 
-              candle['low'], candle['close'], candle['volume'])
+    conn = await asyncpg.connect(DATABASE_URL)
+    try:
+        await conn.execute("""
+            INSERT INTO candles (symbol, time, open, high, low, close, volume)
+            VALUES ($1, $2, $3, $4, $5, $6, $7)
+            ON CONFLICT (symbol, time) 
+            DO UPDATE SET 
+                open = EXCLUDED.open,
+                high = EXCLUDED.high,
+                low = EXCLUDED.low,
+                close = EXCLUDED.close,
+                volume = EXCLUDED.volume
+        """, symbol, candle['time'], candle['open'], candle['high'], 
+             candle['low'], candle['close'], candle['volume']
         )
-        await db.commit()
+    finally:
+        await conn.close()
 
 async def get_candles(symbol, limit):
-    async with aiosqlite.connect('candles.db') as db:
-        cursor = await db.execute("""
+    conn = await asyncpg.connect(DATABASE_URL)
+    try:
+        rows = await conn.fetch("""
             SELECT * FROM candles 
-            WHERE symbol = ? 
+            WHERE symbol = $1 
             ORDER BY time DESC 
-            LIMIT ?
-        """, (symbol, limit)
-        )
-        rows = await cursor.fetchall()
-        return rows
+            LIMIT $2
+        """, symbol, limit)
+        return [dict(row) for row in rows]
+    finally:
+        await conn.close()
